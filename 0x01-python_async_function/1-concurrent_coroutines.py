06dba6b12e188f82fd56f1a0367af6da9801174f
ruizdani301 <ruizdani301@gmail.com>
2022-08-08 22:09:13 -0500

    se corrige linea 12

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,11 +7,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """write an async routine called wait_n that takes in 2 int argument"""
-    i = 0
+
     list: List[float] = []
-    while (i < 0):
+    while (n > 0):
         valor = await (wait_random(max_delay))
         list.append(valor)
-        i += 1
+        n = n - 1
     list = sorted(list)
     return list
