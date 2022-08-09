39b3e5d1ba27411f3b6a02438ecc589690eff0fc
ruizdani301 <ruizdani301@gmail.com>
2022-08-08 22:48:08 -0500

    task-4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """async routine called"""
 from typing import List
-task_wait_random = __import__('3-task').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
