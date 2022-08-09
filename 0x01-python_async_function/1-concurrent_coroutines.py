753ac0cb9c3e10efa5af7c48001a7d92098aa3de
ruizdani301 <ruizdani301@gmail.com>
2022-08-08 20:04:52 -0500

    task-1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Let's execute multiple coroutines at the same time with async"""
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay) -> float:
+    """write an async routine called wait_n that takes in 2 int argument"""
+    list: List[float] = []
+    while n >= 0:
+        valor = await (wait_random(max_delay))
+        list.append(valor)
+        i += 1
+    list = sorted(list)
+    return list
