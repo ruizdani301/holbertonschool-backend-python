1adfe33b8e4101e78098079d866223dd85011fe1
ruizdani301 <ruizdani301@gmail.com>
2022-08-09 16:00:05 -0500

    se modifica linea 9

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """Import async_generator from the previous task and then write a
    coroutine called async_comprehension
 """
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """The coroutine will collect 10 random numbers using an async
         comprehensing over async_generator
     """
