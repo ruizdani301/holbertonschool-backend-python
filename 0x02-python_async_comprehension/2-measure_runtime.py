ca1daeac5cb68b79ded691905c92767d00d17f7f
ruizdani301 <ruizdani301@gmail.com>
2022-08-09 20:23:45 -0500

    se convierte a una comprehension list

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,11 +12,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measure_runtime should measure the total runtime and return it."""
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-        )
+    await asyncio.gather(*(async_comprehension() for x in range(4)))
     end = time.time()
     return end - start
